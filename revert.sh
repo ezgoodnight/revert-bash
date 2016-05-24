49b6affa74c0f0a25b7cfcdd540ca405a1d2014c
Eric Z Goodnight <ezgoodnight@gmail.com>
2016-05-24 13:37:18 -0400

    some cleanup

--- revert.sh
+++ revert.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-#n1=\'
 original_string="$@"
-backup_string=""
 repl_loc="/home/hogwarts/"
 new_loc="/media/backup/"
 
 result_str="${original_string/$repl_loc/$new_loc}"
-echo "$result_str"
-echo "$original_string"
-
 cp "$result_str" "$original_string"
