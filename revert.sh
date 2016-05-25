bcfbeaeb1e2305aded0aaba3291cb4a7be63a49d
Eric Z Goodnight <ezgoodnight@gmail.com>
2016-05-25 13:54:14 -0400

    exit on empty argument and no file

--- revert.sh
+++ revert.sh
@@ -5,4 +5,15 @@ repl_loc="/home/hogwarts/"
 new_loc="/media/backup/"
 
 result_str="${original_string/$repl_loc/$new_loc}"
-cp "$result_str" "$original_string"
+
+if [ -z "$original_string" ]; then
+    echo "Can't revert an empty file"
+    exit
+else
+    echo "$result_str"
+    if [ -e "$result_str" ]; then
+        cp -rp "$result_str" "$original_string"
+    else
+        echo "No file to revert, sorry."
+    fi
+fi
