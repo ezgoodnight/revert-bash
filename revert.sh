9fcb884e250b6fde8fce55e79581d5442606231e
Eric Z Goodnight <ezgoodnight@gmail.com>
2016-06-08 15:21:24 -0400

    some style revisions and added prompt and check

--- revert.sh
+++ revert.sh
@@ -6,14 +6,16 @@ new_loc="/media/backup/"
 
 result_str="${original_string/$repl_loc/$new_loc}"
 
-if [ -z "$original_string" ]; then
-    echo "Can't revert an empty file"
-    exit
+if [ -z "${original_string}" ]; then
+  echo "Can't revert an empty file"
+  exit
 else
-    echo "$result_str"
-    if [ -e "$result_str" ]; then
-        cp -rp "$result_str" "$original_string"
-    else
-        echo "No file to revert, sorry."
-    fi
+  echo "${result_str}"
+#  echo "${original_string}"
+  if [ -e "${result_str}" ]; then
+    cp -rpif "${result_str}" "${original_string}"
+    ls -la "${original_string}"
+  else
+    echo "No file to revert, sorry."
+  fi
 fi
